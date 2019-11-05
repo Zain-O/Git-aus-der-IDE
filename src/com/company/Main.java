d68aa9948fe1fa2a3b6d3d5ee25f5fbcc75a8039
Zain <zainalabedin.omran@jambit.com>
2019-11-05 15:12:11 +0100

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner eingabe = new Scanner(System.in);
+        System.out.println("gib wie viel stellen willst du zuverschieben ein ");
+        int schlussel = eingabe.nextInt();
+        System.out.println("Schreib bitte dein Text zu Verschlüsselen : ");
+        String text = eingabe.next();
+
+        String result = "";
+        for (int i = 0; i < text.length(); i++) {
+            int myassci = text.charAt(i);
+            int sum = myassci + schlussel;
+            char encodedChar = (char) sum;
+            result += encodedChar;
+        }
+        System.out.println("das ist dein verschlüsselte Text " + result);
+
+        String decodedStr = "";
+        for (int i = 0; i < result.length(); i++) {
+            int myassci = result.charAt(i);
+            int sum = myassci - schlussel;
+            char encodedChar = (char) sum;
+            decodedStr += encodedChar;
+        }
+
+        System.out.println(decodedStr);
+    }
+}
+
